216f6bf2b0b9878cc1083295817951f1cc48a132
jilvke <mzxlgx@foxmail.com>
2018-11-08 15:27:19 +0800

    组件配合

--- eurekaclient/src/main/java/com/jilvke/eureka/client/EurekaClientApplication.java
+++ eurekaclient/src/main/java/com/jilvke/eureka/client/EurekaClientApplication.java
@@ -11,12 +11,14 @@ package com.jilvke.eureka.client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RefreshScope
 @RestController
 public class EurekaClientApplication {
     public static void main(String[] args) {
@@ -28,6 +30,6 @@ public class EurekaClientApplication {
 
     @RequestMapping("/")
     public String home() {
-        return "hello world from port " + port;
+        return "hello world from port :" + port;
     }
 }
