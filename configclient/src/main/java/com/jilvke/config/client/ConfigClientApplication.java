0c42325ae1f08cb66fff60cb8fdab5c4d9abf7ea
jilvke <mzxlgx@foxmail.com>
2018-11-08 15:25:40 +0800

    组件配合

--- configclient/src/main/java/com/jilvke/config/client/ConfigClientApplication.java
+++ configclient/src/main/java/com/jilvke/config/client/ConfigClientApplication.java
@@ -3,12 +3,14 @@ package com.jilvke.config.client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RefreshScope
 @RestController
 public class ConfigClientApplication {
 
@@ -16,11 +18,19 @@ public class ConfigClientApplication {
         SpringApplication.run(ConfigClientApplication.class, args);
     }
 
-    @Value("${hello}")
+   // @Value("${hello}")
     String hello;
 
     @RequestMapping(value = "/hello")
     public String hello() {
         return hello;
+    }
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/")
+    public String home() {
+        return "hello world from port :" + port;
     }
 }
