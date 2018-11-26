7da05858c8a817bb8b6a60e5e001773ff538b0fc
jilvke <mzxlgx@foxmail.com>
2018-11-26 14:17:29 +0800

    联系

--- feignclient/src/main/java/com/jilvke/feign/client/WebController.java
+++ feignclient/src/main/java/com/jilvke/feign/client/WebController.java
@@ -1,10 +1,12 @@
+
 package com.jilvke.feign.client;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
 
 /**
  * @ClassName :  WebController
@@ -13,17 +15,28 @@ import org.springframework.web.bind.annotation.RestController;
  * @CreateDate: 2018/11/07
  * @Version: 1.0
  */
+
 @RestController
 public class WebController {
     @Autowired
     ConfigClientService configClientService;
+/*    @Autowired
+    private UploadService uploadService;*/
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @RequestMapping(value = "", method = RequestMethod.GET )
     public String sayHello() {
         return configClientService.sayHello();
     }
+/*    @RequestMapping(value = "/upload",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String testHandleFileUpload( @NonNull  MultipartFile file) {
+        if (file == null ){
+            System.out.println(".........................");
+        }
+        return uploadService.handleFileUpload(file);
+    }*/
 
     public String serviceFailure() {
         return "hello world service is not available !";
     }
 }
+
--- zuulservice/src/main/java/com/jilvke/zuul/service/ZuulserviceApplication.java
+++ zuulservice/src/main/java/com/jilvke/zuul/service/ZuulserviceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @Author: JILVKE
@@ -15,5 +16,9 @@ public class ZuulserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulserviceApplication.class, args);
+	}
+	@Bean
+	public AccessFilter accessFilter() {
+		return new AccessFilter();
 	}
 }
